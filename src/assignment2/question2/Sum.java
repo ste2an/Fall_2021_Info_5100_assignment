16267336b7f262c88deccfe193eb5ddefd23bb98
ste2an <stefanwang2020@gmail.com>
2021-10-12 19:58:36 -0700

    solution for assignment2- Q2

---
+++ src/assignment2/question2/Sum.java
@@ -0,0 +1,18 @@
+package assignment2.question2;
+
+public class Sum {
+    public Sum() {
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+    public double add(double a, double b){
+        return a + b;
+    }
+}
